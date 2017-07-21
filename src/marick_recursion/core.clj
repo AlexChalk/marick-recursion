39b713749e021e698193e5f8b02fcda5f7c2773c
Alex Chalk <alexchalk17@gmail.com>
2017-07-21 13:17:15 -0400

    Add second `factorial` implementation using tail recursion
    Clean up directory by removing temp files.

--- src/marick_recursion/core.clj
+++ src/marick_recursion/core.clj
@@ -4,6 +4,15 @@
   (fn [arg]
     (if (> 2 arg)
       1 
-      (* arg
-         (factorial
-           (- arg 1))))))
+      (* arg (factorial (- arg 1))))))
+
+(def factorial2-1
+  (fn [arg memo]
+    (if (> 2 arg)
+      memo
+      (recur (dec arg)
+             (* arg memo)))))
+
+(def factorial2
+  (fn [arg]
+      (factorial2-1 arg 1)))
--- test/marick_recursion/core_test.clj
+++ test/marick_recursion/core_test.clj
@@ -9,3 +9,11 @@
     (factorial 4) => 24
     (factorial 5) => 120
     (factorial 6) => 720))
+
+(facts "about `factorial2`"
+  (fact "it returns the factorial of its argument"
+    (factorial2 0) => 1
+    (factorial2 1) => 1
+    (factorial2 4) => 24
+    (factorial2 5) => 120
+    (factorial2 6) => 720))
